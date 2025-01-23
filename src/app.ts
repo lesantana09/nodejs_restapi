131c8c73e1d5700c48d9b1615dfb199f165a5560
Leandro Santana <leandro.santana@infosi.com.br>
2025-01-22 23:51:00 -0300

    update swagger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,46 @@
 import { fastify } from 'fastify'
 import cookie from '@fastify/cookie'
 import { transactionsRoutes } from './routes/transactions'
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import {
+  ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
-export const app = fastify()
+export const app = fastify({ logger: true }).withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, { origin: '*' })
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Nodejs Rest API',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        }
+      }
+    },
+    security: [{ bearerAuth: [] }],   
+  },
+  transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+
+})
 
 app.register(cookie)
 app.register(transactionsRoutes, { prefix: 'transactions' })
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,15 +3,21 @@ import { knex } from '../database'
 import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 import { checkSessionIdExists } from '../middlewares/check_session_id_exists'
+import { FastifyTypedInstance } from '../@types/types'
 
-export async function transactionsRoutes(app: FastifyInstance) {
+export async function transactionsRoutes(app: FastifyTypedInstance) {
   app.addHook('preHandler', async (request, reply) => {
     if (!request.method) {
       checkSessionIdExists(request, reply)
     }
   })
 
-  app.get('/', async (request) => {
+  app.get('/', {
+    schema: {
+      description: 'List transactions',
+      tags: ['transactions']
+    }
+  }, async (request) => {
     const { sessionId } = request.cookies
     const transactions = await knex('transactions')
       .select('*')
@@ -19,7 +25,12 @@ export async function transactionsRoutes(app: FastifyInstance) {
     return { transactions }
   })
 
-  app.get('/:id', async (request, reply) => {
+  app.get('/:id', {
+    schema: {
+      description: 'Get transaction',
+      tags: ['transactions'],
+    }
+  }, async (request, reply) => {
     const getTransactionSchema = z.object({
       id: z.string().uuid(),
     })
@@ -36,23 +47,38 @@ export async function transactionsRoutes(app: FastifyInstance) {
     return { transaction }
   })
 
-  app.get('/summary', async (request) => {
-    const { sessionId } = request.cookies
-    const summary = await knex('transactions')
-      .where('session_id', sessionId)
-      .sum('amount', { as: 'total' })
-      .first()
-    return { summary }
-  })
-
-  app.post('/', async (request, reply) => {
-    const createTransactionSchema = z.object({
-      title: z.string(),
-      amount: z.number(),
-      type: z.enum(['credit', 'debit']),
+  app.get('/summary', {
+    schema: {
+      description: 'Get User summary of transaction',
+      tags: ['transactions'],
+    }
+  },
+    async (request) => {
+      const { sessionId } = request.cookies
+      const summary = await knex('transactions')
+        .where('session_id', sessionId)
+        .sum('amount', { as: 'total' })
+        .first()
+      return { summary }
     })
 
-    const { title, amount, type } = createTransactionSchema.parse(request.body)
+  app.post('/', {
+    schema: {
+      description: 'Create a new transaction',
+      tags: ['transactions'],
+      body: z.object({
+        title: z.string().default("Minha Despesa"),
+        amount: z.number().default(100),
+        type: z.enum(['credit', 'debit']).default('debit'),
+      }),
+      response: {
+        201: z.null().describe("User created successfully")
+      }
+    }
+  }, async (request, reply) => {
+
+
+    const { title, amount, type } = request.body
 
     let sessionId = request.cookies.sessionId
 
